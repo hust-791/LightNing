a66070ac79af06067da6c75e81ed09c11f2b5139
yuyan <yuyan@DESKTOP-T9GC4KM>
2024-09-17 23:05:28 +0800

    adw

--- LightNing/src/LN/Renderer/Renderer.cpp
+++ LightNing/src/LN/Renderer/Renderer.cpp
@@ -18,12 +18,12 @@ namespace LN {
 	{
 	}
 
-	void Renderer::Submit(const Ref<Shader>& shader, const Ref<VertexArray>& vertxeArray)
+	void Renderer::Submit(const Ref<Shader>& shader, const Ref<VertexArray>& vertxeArray,const glm::mat4& transform)
 	{
 		shader->Bind();
 		shader->SetUniform1i("u_Texture", 1);
 		shader->SetUniform4f("u_Color", 1.0f, 1.0f, 1.0f, 1.0f);
-		shader->SetUniformMatrix4fv("u_Transform", 1, GL_FALSE, glm::mat4(1.0));
+		shader->SetUniformMatrix4fv("u_Transform", 1, GL_FALSE, transform);
 		shader->SetUniformMatrix4fv("u_ViewProjection", 1, GL_FALSE, m_SenceData->ViewProjectionMatrix);
 		vertxeArray->Bind();
 		RenderCommand::DrawIndexed(vertxeArray);
--- LightNing/src/LN/Renderer/Renderer.h
+++ LightNing/src/LN/Renderer/Renderer.h
@@ -13,7 +13,7 @@ namespace LN
 		static void BeginScene(Camera* camera);
 		static void EndScene();
 
-		static void Submit(const Ref<Shader>& shader, const Ref<VertexArray>& vertxeArray);
+		static void Submit(const Ref<Shader>& shader, const Ref<VertexArray>& vertxeArray, const glm::mat4& transform = glm::mat4(1.0f));
 
 		inline static RendererAPI::API GetAPI(){ return RendererAPI::GetAPI(); }
 
